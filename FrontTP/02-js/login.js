8d6324dbb9d0676d38584fc20b30f83a48f9a891
fm <fm>
2019-06-15 22:32:21 -0300

    TP Front
    Estructrura de directorios, Packages, login incompleto

---
+++ FrontTP/02-js/login.js
@@ -0,0 +1,13 @@
+"use strict";
+var login;
+(function (login) {
+    var Url = "http://localhost:80/LaComanda/login";
+    // function Logear(){
+    //     let user = String($("#txtUserName").val());
+    //     let pass = String($("#txtUserPass").val());
+    //     let jsonObj =
+    //     $.post(Url, jsonObj, function(data, status){                                 
+    //         console.log(data);
+    //     });
+    // }
+})(login || (login = {}));
---
+++ FrontTP/03-scripts/login.ts
@@ -0,0 +1,17 @@
+namespace login{
+    
+
+
+    const Url = "http://localhost:80/LaComanda/login"
+    
+    // function Logear(){
+    //     let user = String($("#txtUserName").val());
+    //     let pass = String($("#txtUserPass").val());
+
+    //     let jsonObj =
+
+    //     $.post(Url, jsonObj, function(data, status){                                 
+    //         console.log(data);
+    //     });
+    // }
+}
